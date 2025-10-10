87cdd4e6ae7e76ef0e8a06d6d06c16c77ce4415b
Oksana Kuziv <oksanakuziv25@gmail.com>
2025-10-10 21:10:54 +0200

    minor UI fixes

--- src/pages/PricesPage/components/PricesList.tsx
+++ src/pages/PricesPage/components/PricesList.tsx
@@ -24,7 +24,7 @@ export const PricesList: React.FC = () => {
       {services.map((service, index) => (
         <div
           key={index}
-          className="max-w-3xl mx-auto p-4 border-b border-b-primary w-full flex justify-between items-center py-4 text-left text-lg"
+          className="max-w-3xl mx-auto border-b border-b-primary w-full flex justify-between items-center py-4 text-left text-lg"
         >
           <span className="text-textBlack">{service.category}</span>
           <span className="text-textGray">{service.price}</span>
